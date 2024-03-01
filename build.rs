371edec10ef054677ae2160bc8ac8d536aed6b93
Chengdong Li <chengdongli@optimatist.com>
2024-03-01 16:11:50 +0800

    Update build.rs for update git submodules.

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     // Update the psh-proto submodule
     let _ = Command::new("git")
-        .args(["submodule", "update", "--remote", "--merge"])
+        .args(["submodule", "update", "--init", "--recursive"])
         .status();
 
     tonic_build::compile_protos("src/services/proto/psh.proto").unwrap();
