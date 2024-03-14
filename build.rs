32bf3957b89c8eb9f06b3ec2248bbf88fffe46ca
Chengdong Li <chengdongli@optimatist.com>
2024-03-14 11:47:50 +0800

    Add LGPL-3.0 license

--- build.rs
+++ build.rs
@@ -1,3 +1,17 @@
+// Copyright (c) 2023-2024 Optimatist Technology Co., Ltd. All rights reserved.
+// DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// This file is part of PSH.
+//
+// PSH is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// PSH is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with Perf-event-rs. If not,
+// see <https://www.gnu.org/licenses/>.
+
 use std::process::Command;
 
 fn main() {
