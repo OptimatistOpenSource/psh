39eb6fa82e9c68323f53105d7eb3287d664fe4a5
Chengdong Li <chengdongli@optimatist.com>
2024-03-20 15:33:14 +0800

    Add an example of get_interrupt_info.

---
+++ examples/wasi/get_interrupts_info/src/main.rs
@@ -0,0 +1,28 @@
+#[rustfmt::skip]
+mod bindings;
+
+use crate::bindings::psh::profiling::interrupts::{get_interrupts_info, get_interrupts_stat};
+
+fn main() {
+    println!("Example: get_interrupts_info");
+
+    let interrupts = get_interrupts_info().unwrap();
+    for interrupt in interrupts {
+        println!("{:?}", interrupt);
+    }
+
+    let mut c = 0;
+    loop {
+        let interrupts_stat = get_interrupts_stat().unwrap();
+        for stat in interrupts_stat {
+            println!("{:?}", stat);
+        }
+        println!();
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        c += 1;
+        if c > 3 {
+            break;
+        }
+    }
+}
