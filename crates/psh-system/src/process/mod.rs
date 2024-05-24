ef22c8bc3c49ab157414d5500d78c80b43bfc899
dearfl <i@flr.me>
2024-05-24 10:01:40 +0800

    rename global.rs to handle.rs

--- crates/psh-system/src/process/mod.rs
+++ crates/psh-system/src/process/mod.rs
@@ -12,5 +12,5 @@
 // You should have received a copy of the GNU Lesser General Public License along with Perf-event-rs. If not,
 // see <https://www.gnu.org/licenses/>.
 
-pub(crate) mod global;
+pub(crate) mod handle;
 pub use procfs::process::{ProcState, Process};
