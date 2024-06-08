40312a0d952578a128b29b78adb72d4657f62c58
Chengdong Li <chengdongli@optimatist.com>
2024-06-08 11:05:05 +0800

    Fixed a typo in the license inside each resource file.

--- crates/op/host-op-system/src/rps/mod.rs
+++ crates/op/host-op-system/src/rps/mod.rs
@@ -9,7 +9,7 @@
 // PSH is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
 // the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License along with Perf-event-rs. If not,
+// You should have received a copy of the GNU Lesser General Public License along with Performance Savior Home (PSH). If not,
 // see <https://www.gnu.org/licenses/>.
 
 use crate::profiling::system::rps::{self, RpsInfo as GuestRpsInfo, RpsQueue as GuestRpsQueue};
