2d90955ee0d7ad372b9f7336bbf4da09b8800df0
saying121 <saying121@outlook.com>
2024-07-29 13:48:54 +0800

    feat(rpc): add luoxiao rpc method

--- src/services/rpc.rs
+++ src/services/rpc.rs
@@ -11,24 +11,72 @@
 //
 // You should have received a copy of the GNU Lesser General Public License along with Performance Savior Home (PSH). If not,
 // see <https://www.gnu.org/licenses/>.
+use anyhow::Result;
 
-#[allow(unused_imports)]
-use super::pb::psh_service_client::PshServiceClient;
-#[allow(unused_imports)]
-use super::pb::PshResponse;
+use crate::services::{
+    host_info::RawInfo,
+    pb::{psh_service_client::PshServiceClient, HostInfoRequest},
+};
 
-// TODO Chengdong Li
-// Define RPC methods here.
+#[derive(Clone, Debug)]
+pub struct Luoxiao {
+    client: PshServiceClient<tonic::transport::Channel>,
+}
+
+impl Luoxiao {
+    pub async fn new(addr: &str) -> Result<Self> {
+        let client: PshServiceClient<tonic::transport::Channel> =
+            PshServiceClient::connect(format!("https://{}", addr)).await?;
+        Ok(Self { client })
+    }
+
+    pub async fn send_info(&mut self, token: String) -> Result<()> {
+        let info: HostInfoRequest = RawInfo::new(token).into();
+
+        let resp = self.client.send_host_info(info).await?;
+
+        let resp = resp.get_ref();
+
+        tracing::info!("{:?}", resp.message);
+
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod rpc_tests {
     use std::future::Future;
 
-    use super::super::pb::psh_service_server::{PshService, PshServiceServer};
-    use super::*;
+    use sysinfo::System;
     use tokio::sync::oneshot;
     use tonic::{transport::Server, Request, Response, Status};
+
+    use self::psh_service_client::PshServiceClient;
+    use crate::{
+        infra::{option::WrapOption, result::WrapResult},
+        services::{
+            pb::{
+                psh_service_server::{PshService, PshServiceServer},
+                *,
+            },
+            rpc::Luoxiao,
+        },
+    };
+
     static ADDR: &str = "[::1]:50051";
+    static ADDR_INFO: &str = "[::1]:50052";
+
+    #[allow(dead_code)]
+    const ADDR_LUOXIAO: &str = "[::1]:7878";
+
+    #[ignore]
+    #[tokio::test]
+    async fn test_send() -> anyhow::Result<()> {
+        let mut lx = Luoxiao::new(ADDR_LUOXIAO).await?;
+        lx.send_info("psh token".to_owned()).await?;
+
+        Ok(())
+    }
 
     // For testing purpose, implement a simple heartbeat RPC at server side.
     #[derive(Debug, Default)]
@@ -44,6 +92,29 @@ mod rpc_tests {
 
             Ok(Response::new(resp))
         }
+
+        async fn send_host_info(
+            &self,
+            request: tonic::Request<HostInfoRequest>,
+        ) -> std::result::Result<tonic::Response<HostInfoResponse>, tonic::Status> {
+            let req = request.into_inner();
+            if let Some(ip) = req.ip_addr {
+                match ip {
+                    host_info_request::IpAddr::Ipv4(v4) => {
+                        let v4 = std::net::Ipv4Addr::from_bits(v4);
+                        println!("{}", v4);
+                    }
+                    host_info_request::IpAddr::Ipv6(_v6) => {
+                        println!("v6");
+                    }
+                }
+            }
+            let resp = HostInfoResponse {
+                errno: 0,
+                message: "ok".to_owned().wrap_some(),
+            };
+            tonic::Response::new(resp).wrap_ok()
+        }
     }
 
     async fn server_shutdown(tx: oneshot::Sender<()>, last_test: impl Future<Output = ()>) {
@@ -52,8 +123,11 @@ mod rpc_tests {
         let _ = tx.send(());
     }
 
-    async fn server_setup(rx: oneshot::Receiver<()>) -> Result<(), Box<dyn std::error::Error>> {
-        let addr = ADDR.parse()?;
+    async fn server_setup(
+        rx: oneshot::Receiver<()>,
+        addr: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let addr = addr.parse()?;
         let heartbeat = MyPshService::default();
 
         Server::builder()
@@ -74,16 +148,52 @@ mod rpc_tests {
         assert_eq!(resp.get_ref().resp, "beep");
     }
 
+    async fn test_send_info(
+        client: impl Future<Output = ClientChannelResult>,
+        req: impl tonic::IntoRequest<HostInfoRequest>,
+    ) {
+        let resp = client.await.unwrap().send_host_info(req).await.unwrap();
+
+        assert_eq!(resp.get_ref().message.as_ref().unwrap(), "ok");
+    }
+
+    async fn server_shutdown_info(tx: oneshot::Sender<()>, last_test: impl Future<Output = ()>) {
+        last_test.await;
+        // Send a signal to trigger shutdown
+        let _ = tx.send(());
+    }
+
+    #[tokio::test]
+    async fn test_psh_send_info() {
+        let (tx, rx) = oneshot::channel();
+        let server = server_setup(rx, ADDR_INFO);
+        let client = PshServiceClient::connect(format!("http://{}", ADDR_INFO));
+        let info_req = HostInfoRequest {
+            token: "token".to_owned(),
+            ip_addr: host_info_request::IpAddr::Ipv4(0xFF00FF00).wrap_some(),
+            os: System::name(),
+            hostname: "MTS-MILA".to_owned().wrap_some(),
+            architecture: System::cpu_arch(),
+            kernel_version: System::kernel_version(),
+        };
+
+        let heartbeat = test_send_info(client, info_req);
+        let shutdown = server_shutdown_info(tx, heartbeat);
+
+        let (ser_status, _) = tokio::join!(server, shutdown);
+        assert!(ser_status.is_ok());
+    }
+
     #[tokio::test]
     async fn test_psh_rpc_heartbeat() {
         // Create a oneshot channel to signal shutdown
         let (tx, rx) = oneshot::channel();
-        let server = server_setup(rx);
+        let server = server_setup(rx, ADDR);
         let client = PshServiceClient::connect(format!("http://{}", ADDR));
         let heartbeat = test_heartbeat(client);
         let shutdown = server_shutdown(tx, heartbeat);
 
         let (ser_status, _) = tokio::join!(server, shutdown);
-        assert_eq!(ser_status.unwrap(), ());
+        assert!(ser_status.is_ok());
     }
 }
