7487de0242c86f2c80f0fb5d7567ff2495c470c3
Thaumy <thaumy@outlook.com>
2024-11-07 11:28:03 +0800

    add `TaskRuntime`

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -19,6 +19,86 @@ mod state;
 #[cfg(test)]
 mod tests;
 
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+use std::thread;
+use std::thread::JoinHandle;
+
+use anyhow::Context;
+use anyhow::Result;
 pub use builder::PshEngineBuilder;
 pub use engine::PshEngine;
 pub use state::PshState;
+
+pub struct Task {
+    pub wasm_component: Vec<u8>,
+    pub wasm_component_args: Vec<String>,
+}
+
+pub struct TaskRuntime {
+    tx: Sender<Task>,
+    rx: Option<Receiver<Task>>,
+    len: Arc<AtomicUsize>,
+}
+
+impl TaskRuntime {
+    pub fn new() -> Result<Self> {
+        let (tx, rx) = channel();
+
+        Ok(Self {
+            tx,
+            rx: Some(rx),
+            len: Arc::new(AtomicUsize::new(0)),
+        })
+    }
+
+    pub fn schedule(&mut self, task: Task) -> Result<()> {
+        self.len.fetch_add(1, Ordering::Release);
+        self.tx.send(task)?;
+        Ok(())
+    }
+
+    #[expect(dead_code)]
+    pub fn is_idle(&self) -> bool {
+        let len = self.len.load(Ordering::Acquire);
+        len == 0
+    }
+
+    pub fn spawn(&mut self) -> Result<JoinHandle<()>> {
+        let rx = match self.rx.take() {
+            Some(rx) => rx,
+            None => panic!("twice spawned"),
+        };
+
+        let envs: Vec<(String, String)> = std::env::vars().collect();
+
+        let len = self.len.clone();
+        let handle = thread::spawn(move || {
+            while let Ok(task) = rx.recv() {
+                let engine = PshEngineBuilder::new()
+                    .wasi_inherit_stdio()
+                    .wasi_envs(&envs)
+                    .wasi_args(&task.wasm_component_args)
+                    .allow_perf_op(true)
+                    .allow_system_op(true)
+                    .build()
+                    .context("Failed to build PshEngine.");
+
+                match engine {
+                    Ok(mut o) => {
+                        let _ = o.run(&task.wasm_component);
+                    }
+                    Err(e) => eprintln!("{}", e),
+                };
+
+                len.fetch_sub(1, Ordering::Release);
+            }
+        });
+
+        Ok(handle)
+    }
+}
