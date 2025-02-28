742ef9007a45f150b3763d464df7d404093f45e6
Thaumy <thaumy@outlook.com>
2025-02-28 14:25:32 +0800

    Update rust toolchain for nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkgs.url = "github:NixOS/nixpkgs/f4341811740ba37cc17962dd1da929bd32dbeb91"; # 24-8-7
     rust-overlay = {
-      url = "github:oxalica/rust-overlay/57a1564c924ee4acbffe0ad3d65c7e90d3e77cd8"; # 24-9-8
+      url = "github:oxalica/rust-overlay/b4270835bf43c6f80285adac6f66a26d83f0f277"; # 25-2-28
       inputs.nixpkgs.follows = "pkgs";
     };
     flake-utils.url = "github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a"; # 24-3-11
@@ -16,7 +16,7 @@
         overlays = [ (import inputs.rust-overlay) ];
         inherit system;
       };
-      toolchain = pkgs.rust-bin.stable."1.81.0".complete.override {
+      toolchain = pkgs.rust-bin.stable."1.85.0".complete.override {
         extensions = [ "rust-src" ];
         targets = [ "x86_64-unknown-linux-gnu" "wasm32-wasip1" ];
       };
